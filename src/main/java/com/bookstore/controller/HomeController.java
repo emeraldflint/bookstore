3c950b1c34218781bded4761f4000e333175897f
EugeneKasap <Evgeniy.Kasap@diymaxeda.com>
2017-03-06 23:32:07 +0200

    Added skeleton for bookstore project.

---
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.misc.Contended;
+
+/**
+ * Created by ekasap on 06.03.2017.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
