0c9eee60dfcd5eeabb461ba2f28d828ed39f8a9e
EugeneKasap <Evgeniy.Kasap@diymaxeda.com>
2017-03-11 23:08:20 +0200

    Added myAccount page and spring-boot-devtools dependency for hot redeploy.

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -14,4 +14,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/myAccount")
+    public String myAccount(){
+        return "myAccount";
+    }
 }
