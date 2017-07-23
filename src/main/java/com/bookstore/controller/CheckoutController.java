e92d40f331801c01eea4019cc52059e67a1551c2
EugeneKasap <Evgeniy.Kasap@diymaxeda.com>
2017-07-23 14:07:19 +0300

    Added checkout controller and related services.

---
+++ src/main/java/com/bookstore/controller/CheckoutController.java
@@ -0,0 +1,105 @@
+package com.bookstore.controller;
+
+import com.bookstore.domain.*;
+import com.bookstore.service.*;
+import com.bookstore.utility.USConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class CheckoutController {
+    private ShippingAddress shippingAddress = new ShippingAddress();
+    private BillingAddress billingAddress = new BillingAddress();
+    private Payment payment = new Payment();
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private ShippingAddressService shippingAddressService;
+    @Autowired
+    private PaymentService paymentService;
+    @Autowired
+    private BillingAddressService billingAddressService;
+
+    @RequestMapping("/checkout")
+    public String checkout(@RequestParam("id") Long cartId, @RequestParam(value = "missingRequiredField", required = false) boolean missingRequiredField,
+                           Model model, Principal principal) {
+        User user = userService.findByUserName(principal.getName());
+        if (cartId != user.getShoppingCart().getId()) {
+            return "badRequestPage";
+        }
+
+        List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
+
+        if (cartItemList.size() == 0) {
+            model.addAttribute("emptyCart", true);
+            return "forward:/shoppingCart/cart";
+        }
+
+        for (CartItem cartItem : cartItemList) {
+            if (cartItem.getBook().getInStockNumber() < cartItem.getQty()) {
+                return "forward:/shoppingCart/cart";
+            }
+        }
+
+        List<UserShipping> userShippingList = user.getUserShippingList();
+        List<UserPayment> userPaymentList = user.getUserPaymentList();
+
+        if (userPaymentList.size() == 0) {
+            model.addAttribute("emptyPaymentList", true);
+        } else {
+            model.addAttribute("emptyPaymentList", false);
+        }
+
+        if (userShippingList.size() == 0) {
+            model.addAttribute("emptyPaymentList", true);
+        } else {
+            model.addAttribute("emptyPaymentList", false);
+        }
+
+        ShoppingCart shoppingCart = user.getShoppingCart();
+
+        for (UserShipping userShipping : userShippingList) {
+            if (userShipping.isUserShippingDefault()) {
+                shippingAddressService.setByUserShipping(userShipping, shippingAddress);
+            }
+        }
+
+        for (UserPayment userPayment : userPaymentList) {
+            if (userPayment.isDefaultPayment()) {
+                paymentService.setByUserPayment(userPayment, payment);
+                billingAddressService.setByUserBilling(userPayment.getUserBilling(), billingAddress);
+            }
+        }
+
+        model.addAttribute("shippingAddress", shippingAddress);
+        model.addAttribute("payment", payment);
+        model.addAttribute("billingAddress", billingAddress);
+        model.addAttribute("cartItemList", cartItemList);
+        model.addAttribute("shoppingCart", user.getShoppingCart());
+
+        List<String> stateList = USConstants.listOfUSStatesCode;
+        Collections.sort(stateList);
+
+        model.addAttribute("stateList", stateList);
+        model.addAttribute("/classActiveShipping", true);
+
+        if (missingRequiredField) {
+            model.addAttribute("missingRequiredField", true);
+        }
+
+        return "checkout";
+
+    }
+}
---
+++ src/main/java/com/bookstore/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bookstore.service.impl;
+
+import com.bookstore.domain.Payment;
+import com.bookstore.domain.UserPayment;
+import com.bookstore.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public Payment setByUserPayment(UserPayment userPayment, Payment payment) {
+        payment.setType(userPayment.getType());
+        payment.setHolderName(userPayment.getHolderName());
+        payment.setCardNumber(userPayment.getCardNumber());
+        payment.setExpiryMonth(userPayment.getExpiryMonth());
+        payment.setCvc(userPayment.getCvc());
+
+        return payment;
+    }
+}
